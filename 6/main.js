3428d3b07a668ceda8618804d79d41282da7f2a9
fatemeh ezzati <fatemehezzati121@gmail.com>
2023-01-06 05:09:43 +0330

    update and delete

--- 6/main.js
+++ 6/main.js
@@ -1,4 +1,6 @@
+// dynamic code for every kind of user data
 // console.table(userData)
+
 let mainTag = document.createElement('main')
 document.body.appendChild(mainTag);
 mainTag.innerHTML = '<header>userData table  <button class="addData" onclick="addNewData()">add new user</button></header><table id="userData"></table>'
@@ -50,17 +52,63 @@ function generateModal(index, existed) {
     mainTag.appendChild(modal)
 
     modal.innerHTML = `
-        <header>user info</header>
+        <header>user info <button class="cancel" onclick='cancel()'>x</botton> </header>
         <div class='rowInfo'>
             ${Object.keys(userData[index]).map((item, key) => {
-        return (`<label for=${item}>${item}  <input type="text" placeholder=${item} name=${item} value=${existed ? userData[index][item] : ''}  ${item === 'uid' && existed ? 'readOnly' : ""}></label>`)
+        return (`<label for=${item}>${item}  <input type="text" placeholder=${item} name=${item} id=${item}  onchange="handleChange(event,${index})" value=${existed ? userData[index][item] : ''}  ${item === 'uid' && existed ? 'readOnly' : ""}></label>`)
     }).join('')}
         </div>
-        <div class='btnwrapper'><button class="cancel" onclick='cancel()'>cancel</botton> </div>
+        <div class='btnwrapper'>
+            ${existed ? `<button class="delete" onclick='deleteUser(${index})'>delete user</botton>` : ''}
+            <button class="addData" onclick='submit(${existed})'>${existed ? 'update' : 'submit'}</botton> 
+        </div>
         `
 }
 
 function cancel() {
+    newData = {}
     let modal = document.getElementsByClassName('modal')[0]
     modal.remove()
+}
+
+function deleteUser(index) {
+    userData.splice(index, 1)
+    console.table(userData)
+    makeTable()
+    cancel()
+}
+
+let newData = {}
+function handleChange(e, index) {
+    newData[e.target.id] = e.target.value
+    newData.index = index
+}
+
+function submit(existed) {
+    let valid = validation()
+    if (valid) {
+        let index = newData.index
+        delete newData.index
+        if (existed) {
+            for (const key in newData) {
+                userData[index][key] = newData[key];
+            }
+
+        } else {
+            userData.push(newData)
+        }
+
+        makeTable()
+        cancel()
+    }
+}
+
+function validation() {
+    for (const key in newData) {
+        if (!newData[key]) {
+            console.log(newData[key], key)
+        }
+    }
+
+    return true
 }
